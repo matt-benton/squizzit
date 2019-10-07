feff79f8602cfcaae10f93d9349d6aa32f72f839
Matt <mattbenton10@gmail.com>
2019-10-07 16:38:25 -0500

    Add a long quiz to seeder

--- database/seeds/QuizzesTableSeeder.php
+++ database/seeds/QuizzesTableSeeder.php
@@ -11,10 +11,22 @@ class QuizzesTableSeeder extends Seeder
      */
     public function run()
     {
+        $defaultUser = App\User::findOrFail(1);
+
+        // create a quiz with a bunch of questions
+        $longQuiz = factory(App\Quiz::class)->create([
+            'name' => 'Long Quiz',
+            'description' => 'Long quiz containing 75 questions.'
+        ]);
+        $longQuiz->questions()->createMany(
+            factory(App\Question::class, 75)->make([
+                'quiz_id' => $longQuiz->id
+            ])->toArray()
+        );
+        $longQuiz->users()->attach($defaultUser, ['role' => 'maker']);
+
         // create quizzes and attach them to the default user
         $quizzes = factory(App\Quiz::class, 50)->create();
-
-        $defaultUser = App\User::findOrFail(1);
         
         foreach ($quizzes as $quiz) {
             $quiz->users()->attach($defaultUser, ['role' => 'maker']);
