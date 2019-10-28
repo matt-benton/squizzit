0fab668c4e8c7a31d514dc9ef40877d5f06dc1db
Matt <mattbenton10@gmail.com>
2019-10-28 10:58:54 -0500

    Add message when no quiz invites can be shown

--- resources/js/pages/Invites.vue
+++ resources/js/pages/Invites.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="has-background-light">
         <div class="container">
-            <section class="section">
+            <section class="section pb-sm">
+                <p class="is-size-4 has-text-grey">Invites</p>
+            </section>
+            <section class="section" v-if="quizInvites.length > 0">
                 <div class="card mb-md" v-for="inv in quizInvites" :key="inv.id">
                     <header class="card-header">
                         <p class="card-header-title">You have been invited to take a quiz!</p>
@@ -16,6 +19,26 @@
                         <a class="card-footer-item" @click="joinQuiz(inv)">Take Quiz</a>
                         <a class="card-footer-item" @click="decline(inv)">Decline</a>
                     </footer>
+                </div>
+            </section>
+            <section class="columns" v-else>
+                <div class="column is-half is-offset-one-quarter has-text-grey">
+                    <h1 class="is-size-3 pb-sm pt-md">No active invites.</h1>
+                    <h2 class="is-size-4 pb-md">
+                        When someone invites you to a quiz, it will show up here.
+                    </h2>
+                    <router-link to="/quizzes/create" class="button is-rounded is-primary">
+                        <span class="icon is-small">
+                            <i class="fas fa-pen"></i>
+                        </span>
+                        <span>Create Quiz and Invite Friends</span>
+                    </router-link>
+                    <router-link to="/quizzes" class="button is-rounded is-primary is-outlined ml-sm">
+                        <span class="icon is-small">
+                            <i class="fas fa-user-plus"></i>
+                        </span>
+                        <span>Back to My Quizzes</span>
+                    </router-link>
                 </div>
             </section>
         </div>
