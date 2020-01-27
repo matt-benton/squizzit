1fa1a72e9a2c94c197b4fc13d1318de8aeabfd2a
Matt Benton <mattbenton@Luca.local>
2020-01-27 12:18:24 -0600

    Fix conflicts with mobile menu and dropdown menu buttons

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -22,26 +22,26 @@
                 </div> -->
             </div>
             <div class="sm:hidden">
-                <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+                <button @click="menuOpen = !menuOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
                     <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path v-if="isOpen" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+                        <path v-if="menuOpen" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
                         <path v-else d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
                     </svg>
                 </button>
             </div>
         </div>
-        <div class="px-2 pt-2 pb-4 sm:flex sm:p-0">
+        <div :class="menuOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
             <router-link to="/invites" class="block px-2 py-1 text-white rounded hover:bg-gray-800">
                 Invites
                 <span class="tag is-white is-rounded ml-xs" id="quiz-invite-counter" v-show="numQuizInvites > 0">{{ numQuizInvites }}</span>
             </router-link>
 
             <div class="relative ml-6">
-                <button @click="isOpen = !isOpen" class="relative z-10 block h-8">
+                <button @click="emailDropdownOpen = !emailDropdownOpen" class="relative z-10 block h-8">
                     {{ email }}
                 </button>
-                <button v-if="isOpen" @click="isOpen = false" class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button>
-                <div :class="isOpen ? 'block' : 'hidden'" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+                <button v-if="emailDropdownOpen" @click="emailDropdownOpen = false" class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button>
+                <div :class="emailDropdownOpen ? 'block' : 'hidden'" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
                     <a class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white" @click="logout">
                         Logout
                     </a>
@@ -61,7 +61,8 @@
             return {
                 email: localStorage.getItem('email'),
                 quizzesMenuVisible: true,
-                isOpen: false,
+                emailDropdownOpen: false,
+                menuOpen: false,
             }
         },
         created() {
