5408ccea9afdf6a293cfaf79a216493f8097aec1
Matt Benton <mattbenton@Luca.local>
2020-01-31 12:18:14 -0600

    Add button back for removing answer from question

--- resources/js/components/QuestionEditor.vue
+++ resources/js/components/QuestionEditor.vue
@@ -36,6 +36,11 @@
                             <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
                         </svg>
                     </button>
+                    <button class="rounded text-gray-600 p-1 ml-2" @click="removeAnswer(index)">
+                        <svg class="h-5 w-5 fill-current" viewBox="0 0 24 24">
+                            <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
