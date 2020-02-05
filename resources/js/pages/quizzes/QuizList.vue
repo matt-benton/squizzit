0bac4cb5c95c753f5885f6f0561f7f4f0fdbaef3
Matt Benton <mattbenton@Luca.local>
2020-02-05 12:30:07 -0600

    Style quiz list page for the case of no quizzes

--- resources/js/pages/quizzes/QuizList.vue
+++ resources/js/pages/quizzes/QuizList.vue
@@ -16,24 +16,22 @@
                 </div>
             </div>
         </section>
-        <section class="columns" v-else>
-            <div class="column is-half is-offset-one-quarter has-text-grey">
-                <h1 class="is-size-3 pb-sm pt-md">No quizzes available</h1>
-                <h2 class="is-size-4 pb-md">
+        <section class="flex flex-col items-center p-5" v-else>
+            <div class="max-w-xl mt-10">
+                <h1 class="mb-5 text-2xl">No quizzes available</h1>
+                <h3 class="text-md mb-5">
                     To get started, join a quiz or make one yourself.  Once you
                     you have made or joined a quiz, it will show up here.
-                </h2>
-                <router-link to="/quizzes/create" class="button is-rounded is-primary">
-                    <span class="icon is-small">
-                        <i class="fas fa-pen"></i>
-                    </span>
-                    <span>Make a Quiz</span>
+                </h3>
+                <router-link to="/quizzes/create">
+                    <button class="btn btn-dark">
+                        Make a Quiz
+                    </button>
                 </router-link>
-                <router-link to="/invites" class="button is-rounded is-primary is-outlined ml-sm">
-                    <span class="icon is-small">
-                        <i class="fas fa-user-plus"></i>
-                    </span>
-                    <span>Join a Quiz</span>
+                <router-link to="/invites">
+                    <button class="btn btn-secondary">
+                        Join a Quiz
+                    </button>
                 </router-link>
             </div>
         </section>
