f9756053c7faaa34bd5aa93cec63eb5c0182476e
Matt Benton <mattbenton@Luca.local>
2020-02-21 12:19:48 -0600

    Make the quiz results page look better

--- resources/js/pages/quizzes/QuizResults.vue
+++ resources/js/pages/quizzes/QuizResults.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="container mx-auto p-2">
         <section class="section is-small">
-            <h1 class="text-xl text-gray-600 my-6">{{ quiz.name }}</h1>
-            <h3 class="text-md">Your Score: {{ score }}</h3>
-            <p class="text-md my-2">You answered {{ correctAnswers }} out of {{ totalQuestions }} questions correctly.</p>
+            <h1 class="text-2xl text-gray-800 my-5">{{ quiz.name }}</h1>
+            <h3 class="text-md text-gray-700">Your Score: <span class="text-2xl text-blue-600 ml-1">{{ score }}</span></h3>
+            <p class="text-md my-2 text-gray-700">You answered {{ correctAnswers }} out of {{ totalQuestions }} questions correctly.</p>
         </section>
-        <hr class="my-6">
-        <section class="my-8" v-for="(question, questionIndex) in quiz.questions" :key="question.id">
+        <section class="my-5 p-6 shadow-md bg-gray-100" v-for="(question, questionIndex) in quiz.questions" :key="question.id">
             <p class="text-gray-600 flex items-center">
                 <svg class="h-5 w-5 fill-current text-green-500" v-if="questionIsCorrect(question)" id="correct-icon" viewBox="0 0 24 24">
                     <path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M12 20C7.59 20 4 16.41 4 12S7.59 4 12 4 20 7.59 20 12 16.41 20 12 20M16.59 7.58L10 14.17L7.41 11.59L6 13L10 17L18 9L16.59 7.58Z" />
@@ -16,17 +15,15 @@
                 </svg>
                 <span class="ml-1">Question {{ questionIndex + 1 }}</span>
             </p>
-            <p class="my-4">{{ question.text }}</p>
+            <p class="my-2 text-gray-800">{{ question.text }}</p>
             <p v-for="(answer, answerIndex) in question.answers" :key="answer.id" class="ml-2 my-1">
                 <span v-bind:class="{ 
                     'font-medium text-green-500': userSelectedThisAnswer(question, answer) && questionIsCorrect(question) ,
                     'font-medium text-red-500': userSelectedThisAnswer(question, answer) && questionIsIncorrect(question)
                     }">
-                    <span class="text-sm text-gray-600 mr-1">{{ answerIndex + 1 }}.</span>
                     {{ answer.text }}
                 </span>
             </p>
-            <hr class="my-6">
         </section>
     </div>
 </template>
