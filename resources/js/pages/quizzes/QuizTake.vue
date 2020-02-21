2bb36bb9c4e54c9eb159cfe1d4eefdcd7a970c41
Matt Benton <mattbenton@Luca.local>
2020-02-21 12:05:58 -0600

    Make the quiz taking page look a lot better

--- resources/js/pages/quizzes/QuizTake.vue
+++ resources/js/pages/quizzes/QuizTake.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="container mx-auto p-2 md:px-20">
         <div class="my-4">
-            <h3 class="text-lg my-1" id="quiz-title">
+            <h3 class="text-2xl my-1 text-gray-800" id="quiz-title">
                 {{ quiz.name }}
             </h3>
-            <h5 class="text-sm text-gray-600">
+            <h5 class="text-sm text-gray-700">
                 {{ quiz.description }}
             </h5>
         </div>
-        <hr>
-        <section class="py-5" v-for="(question, index) in quiz.questions" :key="question.id">
+        <section class="p-6 bg-gray-100 my-5 shadow-md border-t-4 border-blue-900" v-for="(question, index) in quiz.questions" :key="question.id">
             <p class="text-sm text-gray-600">Question {{ index + 1 }}</p>
-            <p class="my-2">{{ question.text }}</p>
+            <p class="my-2 text-gray-800">{{ question.text }}</p>
             <div class="flex flex-col p-4">
                 <label class="my-1 cursor-pointer" v-for="answer in question.answers" :key="answer.id">
                     <input 
                         type="radio" 
+                        class="mr-2"
                         :name="`question-${ index + 1 }-answers`" 
                         :value="answer.id" 
                         v-model="question.takerAnswer.answer_id"
